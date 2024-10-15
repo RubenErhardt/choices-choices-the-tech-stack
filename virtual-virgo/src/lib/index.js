c6ff761ed8e1b85c323fbe94f689cf0883b3d317
RubenErhardt <rubengamebattles@outlook.com>
2024-10-15 20:52:12 +0200

    Header + Footer
    - remake header
    - remake footer
    - begrijpen astro

---
+++ virtual-virgo/src/lib/index.js
@@ -0,0 +1,12 @@
+
+// This is an array with the data for categories with all the correct id's and slugs
+export const categoriesData = [
+    {"id": 9, "name": "Binnenland", "slug": "binnenland"},
+    {"id": 1010, "name": "Buitenland", "slug": "buitenland"}, 
+    {"id": 7164, "name": "Column", "slug": "column"},
+    {"id": 6, "name": "Economie", "slug": "economie"},
+    {"id": 4, "name": "Kunst & Media", "slug": "kunst-media"},
+    {"id": 3211, "name": "Podcasts", "slug": "podcast"},
+    {"id": 63, "name": "Politiek", "slug": "politiek"},
+    {"id": 94, "name": "Wetenschap", "slug": "wetenschap"},
+];
